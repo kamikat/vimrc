3aa262199e43c569105489c4eac6b65eb5a83cb7
kamikat <kamikat@banana.moe>
2015-06-02 02:11:02 +0800

    robust purge.sh

--- purge.sh
+++ purge.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MODULE=$1
 git submodule deinit $MODULE
-git rm -rf $MODULE
+git rm -rf $MODULE || rm -rf $MODULE
 rm -rf .git/modules/$MODULE
-gsed -i "\@/$MODULE@,+2 d" .gitmodules
+sed -i "\@/$MODULE@,+2 d" .gitmodules
