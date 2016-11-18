5d1d0f6b06742d42d92cbf0fb006f98f7edfc4d8
kamikat <kamikat@banana.moe>
2016-11-18 17:57:16 +0800

    add -f flag to purge script

--- purge.sh
+++ purge.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 MODULE=$1
-git submodule deinit $MODULE
+git submodule deinit -f $MODULE
 git rm -rf $MODULE || rm -rf $MODULE
 rm -rf .git/modules/$MODULE
 sed -i "\@/$MODULE@,+2 d" .gitmodules
